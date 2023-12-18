c6cbce75dfa02a6df0fd860470af8f821b6edda5
seanbsu <122116208+seanbsu@users.noreply.github.com>
2023-12-17 20:39:42 -0700

    add home page routing to logos, header and footer collapse fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
